b992b9a48d9ae48d2ee2820a994ce6ef32f8da3f
ThDat1301 <2051052028dat@ou.edu.vn>
2023-03-03 12:34:30 +0700

    init project

---
+++ HOTELMANAGEMENT/DAL_Hotel/RoomDAL.cs
@@ -0,0 +1,39 @@
+﻿using DTO_Hotel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Hotel
+{
+    public class RoomDAL
+    {
+        public List<DTO_Room> listRoom()
+        {
+            List<DTO_Room> list = new List<DTO_Room>();
+
+            using (var db = new HotelDBEntities() )
+            {
+                var tb = from room in db.Rooms
+                         select room;
+
+                foreach( var i in tb)
+                {
+                    DTO_Room room = new DTO_Room();
+                    room.Room_id = i.id;
+                    room.Room_num = i.num.ToString();
+                    room.Room_status = (bool)i.status;
+                    room.Room_type = i.type;
+                    room.Room_price = (float)i.price;
+                    //room.Room_order_id = (int)i.orderID;
+                    list.Add(room);
+                }
+            }
+
+            return list;
+        }
+
+    }
+}
---
+++ HOTELMANAGEMENT/DTO_Hotel/DTO_Room.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_Hotel
+{
+    public class DTO_Room
+    {
+        private int _Room_id;
+        private string _Room_num;
+        private bool _Room_status;
+        private string _Room_type;
+        private float _Room_price;
+        private int _Room_order_id;
+
+        public int Room_id
+        {
+            get { return _Room_id; }
+            set { _Room_id = value; }
+        }
+
+        public string Room_num
+        {
+            get { return _Room_num; }  
+            set { _Room_num = value; }
+        }
+
+        public bool Room_status
+        { 
+            get { return _Room_status; }
+            set { _Room_status = value; }
+        }
+
+        public string Room_type
+        {
+            get { return _Room_type; }
+            set { _Room_type = value; } 
+        }
+
+        public float Room_price
+        {
+            get { return _Room_price; }
+            set { _Room_price = value; }
+        }
+
+        public int Room_order_id
+        {
+            get { return _Room_order_id;}
+            set { _Room_order_id = value;}
+        }
+    }
+}
